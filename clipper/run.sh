f1929a022090193e97221856a8b5450edd1149ff
Dan Crankshaw <dscrankshaw@gmail.com>
2017-08-22 15:06:19 -0700

    updated docker file

--- clipper/run.sh
+++ clipper/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 8888:8888 -p 1337:1337 clipper/risecamp-exercises
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock \
+  -p 8888:8888 clipper/risecamp-exercises-2017
