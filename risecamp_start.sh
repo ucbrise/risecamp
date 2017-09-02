58dea418b76a9f25ae29800343af5fa5a8821bf3
Jey Kottalam <jey@cs.berkeley.edu>
2017-09-02 22:59:32 +0000

    fix notebook startup

--- risecamp_start.sh
+++ risecamp_start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-source ./ground/ground_start.sh
-source /opt/pywren/pywren_start.sh
-source ./wave/wave_start.sh
+./ground/ground_start.sh
+/opt/pywren/pywren_start.sh
+./wave/wave_start.sh
 
+export BW2_DEFAULT_BANKROLL="/home/$NB_USER/wave/ns.ent"
+export BW2_DEFAULT_ENTITY="/home/$NB_USER/wave/ns.ent"
+export NAMESPACE=$(bw2 i /home/$NB_USER/wave/ns.ent | awk '{if($2~"Alias") print $3}')
+
+cd /home/$NB_USER
 start-notebook.sh
