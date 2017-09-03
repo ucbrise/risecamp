85b9d0dc3cde07449d88a69f14a66d4d96d8a6c7
wk <cwk32@mail.ustc.edu.cn>
2017-09-02 20:36:09 -0700

    ensure Clipper can access docker.sock regardless of mount

--- risecamp_start.sh
+++ risecamp_start.sh
@@ -4,6 +4,23 @@
 /opt/pywren/pywren_start.sh
 ./wave/wave_start.sh
 
+# an enchaned kludge to ensure clipper successfully call docker.sock
+# https://stackoverflow.com/questions/23544282/what-is-the-best-way-to-manage-permissions-for-docker-shared-volumes/28596874#28596874
+TARGET_GID=$(stat -c "%g" /var/run/docker.sock)
+
+EXISTS=$(cat /etc/group | grep $TARGET_GID | wc -l)
+
+# Create new group using target GID and add user
+if [ $EXISTS == "0" ]; then
+  groupadd -g $TARGET_GID risecampdocker
+  gpasswd -a $NB_USER risecampdocker
+else
+  # GID exists, find group name and add
+  GROUP=$(getent group $TARGET_GID | cut -d: -f1)
+  gpasswd -a $NB_USER $GROUP
+fi
+
+
 export BW2_DEFAULT_BANKROLL="/home/$NB_USER/wave/ns.ent"
 export BW2_DEFAULT_ENTITY="/home/$NB_USER/wave/ns.ent"
 export NAMESPACE=$(bw2 i /home/$NB_USER/wave/ns.ent | awk '{if($2~"Alias") print $3}')
