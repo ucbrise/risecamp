697e82cce098436f49db4762c4e18ea1fbe81549
Vikram Sreekanti <vsreekanti@gmail.com>
2017-09-03 21:09:31 -0700

    Fix permissions; minor updates to content

--- ground/aboveground/ground.py
+++ ground/aboveground/ground.py
@@ -77,8 +77,18 @@ class GroundClient:
 
         return r.json()
 
+    def get_le_version(self, lev_id):
+        r = requests.get(self.address + '/versions/lineage_edges/' + str(lev_id))
+
+        return r.json()
+
     def get_lineage_edge_latest(self, le_key):
         r = requests.get(self.address + '/lineage_edges/' + le_key + '/latest')
+
+        return r.json()
+
+    def get_node_adj_le(self, nv_id):
+        r = requests.get(self.address + '/versions/nodes/adjacent/lineage/' + str(nv_id))
 
         return r.json()
 
