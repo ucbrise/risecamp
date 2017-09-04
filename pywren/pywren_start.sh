eb0f7e57a6aaf29eeacea0c2f519a032c97bcadf
Qifan Pu <qifan.pu@gmail.com>
2017-09-04 15:19:52 -0700

    update startup

--- pywren/pywren_start.sh
+++ pywren/pywren_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /opt/pywren
-python config_encoder.py decode $PYWREN_CONFIG_STRING ~/.pywren_config
+python config_encoder.py decode $PYWREN_CONFIG_STRING /home/$NB_USER/.pywren_config
+chmod a+r /home/$NB_USER/.pywren_config
 export PYWREN_LOGLEVEL=ERROR
 pywren deploy_lambda
