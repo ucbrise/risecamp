5b8590d9913b09ff431dde24664df9aa36bfaadb
Jey Kottalam <jey@cs.berkeley.edu>
2017-09-05 22:48:15 +0000

    background pywren and ground startup scripts

--- risecamp_start.sh
+++ risecamp_start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-./ground/ground_start.sh
-/opt/pywren/pywren_start.sh
-./wave/wave_start.sh
+./ground/ground_start.sh &
+/opt/pywren/pywren_start.sh &
 
-# an enchaned kludge to ensure clipper successfully call docker.sock
+# an enhanced kludge to ensure clipper successfully call docker.sock
 # https://stackoverflow.com/questions/23544282/what-is-the-best-way-to-manage-permissions-for-docker-shared-volumes/28596874#28596874
 TARGET_GID=$(stat -c "%g" /var/run/docker.sock)
 
@@ -20,7 +19,7 @@ else
   gpasswd -a $NB_USER $GROUP
 fi
 
-
+./wave/wave_start.sh
 export BW2_DEFAULT_BANKROLL="/home/$NB_USER/wave/ns.ent"
 export BW2_DEFAULT_ENTITY="/home/$NB_USER/wave/ns.ent"
 export NAMESPACE=$(bw2 i /home/$NB_USER/wave/ns.ent | awk '{if($2~"Alias") print $3}')
