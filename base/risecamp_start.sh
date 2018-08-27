8545ac596766c03e9d7e8b6085d34f8a95b9e255
Jey Kottalam <jey@kottalam.net>
2018-08-26 20:52:28 -0700

    split out base notebook

--- base/risecamp_start.sh
+++ base/risecamp_start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [ "${NOTEBOOK_AUTH_TOKEN}" != "" ]; then
-  echo "*** Auth Token: ${NOTEBOOK_AUTH_TOKEN}"
+  echo "****"
+  echo "**** Notebook Login URL: http://127.0.0.1:8888/${NOTEBOOK_BASE_URL}?token=${NOTEBOOK_AUTH_TOKEN}"
+  echo "****"
   NOTEBOOK_FLAGS="${NOTEBOOK_FLAGS} --NotebookApp.token=\"${NOTEBOOK_AUTH_TOKEN}\""
 fi
 
 if [ "${NOTEBOOK_BASE_URL}" != "" ]; then
-  echo "*** Base URL: ${NOTEBOOK_BASE_URL}"
   NOTEBOOK_FLAGS="${NOTEBOOK_FLAGS} --NotebookApp.base_url=\"${NOTEBOOK_BASE_URL}\""
 fi
 
