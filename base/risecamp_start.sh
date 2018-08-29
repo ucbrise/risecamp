e1a603781ff29a6c21edccec4c010cfa70175aa1
Jey Kottalam <jey@cs.berkeley.edu>
2018-08-28 22:11:59 -0700

    add example/

--- base/risecamp_start.sh
+++ base/risecamp_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -o errexit
 
 if [ "${NOTEBOOK_AUTH_TOKEN}" != "" ]; then
   echo "****"
@@ -11,5 +12,6 @@ if [ "${NOTEBOOK_BASE_URL}" != "" ]; then
   NOTEBOOK_FLAGS="${NOTEBOOK_FLAGS} --NotebookApp.base_url=\"${NOTEBOOK_BASE_URL}\""
 fi
 
-cd /home/$NB_USER
+cd "/home/$NB_USER"
+chown -R "$NB_USER:users" .
 start-notebook.sh ${NOTEBOOK_FLAGS}
