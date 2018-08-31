dc74e2b3a5495b27af94cf35bc0644c50deeb3ab
Michael Andersen <m.andersen@cs.berkeley.edu>
2018-08-30 18:05:11 -0700

    Everyone forget you saw that

--- last_time/risecamp_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-./ground/ground_start.sh &
-/opt/pywren/pywren_start.sh &
-source ./wave/wave_start.sh
-
-start-notebook.sh
--- last_time/wave/wave_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-daemon --name ragent --respawn -- /bin/ragent client /etc/rise_entity.ent ragent.cal-sdb.org:28590 MT3dKUYB8cnIfsbnPrrgy8Cb_8whVKM-Gtg2qd79Xco= 0.0.0.0:28589
-python2 /usr/local/bin/getentity.py
-export BW2_DEFAULT_BANKROLL="/home/$NB_USER/wave/ns.ent"
-export BW2_DEFAULT_ENTITY="/home/$NB_USER/wave/ns.ent"
-export NAMESPACE=$(bw2 i /home/$NB_USER/wave/ns.ent | awk '{if($2~"Alias") print $3}')
-rm -f .bw2bind.log
