49cbc74147d5b9755f955b2c38a65552f0c60c12
Jey Kottalam <jey@cs.berkeley.edu>
2018-10-03 15:19:29 -0700

    add nginx to base

--- base/risecamp_start.sh
+++ base/risecamp_start.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -o errexit
 
-if [ "${NOTEBOOK_AUTH_TOKEN}" != "" ]; then
-  echo "****"
-  echo "**** Notebook Login URL: http://127.0.0.1:8888/${NOTEBOOK_BASE_URL}?token=${NOTEBOOK_AUTH_TOKEN}"
-  echo "****"
-  NOTEBOOK_FLAGS="${NOTEBOOK_FLAGS} --NotebookApp.token=\"${NOTEBOOK_AUTH_TOKEN}\""
-fi
+NOTEBOOK_FLAGS="${NOTEBOOK_FLAGS} --NotebookApp.base_url=\"${CONTAINER_BASE_URL}/jupyter\""
 
-if [ "${NOTEBOOK_BASE_URL}" != "" ]; then
-  NOTEBOOK_FLAGS="${NOTEBOOK_FLAGS} --NotebookApp.base_url=\"${NOTEBOOK_BASE_URL}\""
+if [ "${CONTAINER_AUTH_TOKEN}" != "" ]; then
+  echo "****"
+  echo "**** Notebook Login URL: http://127.0.0.1:8080/${CONTAINER_BASE_URL}?token=${CONTAINER_AUTH_TOKEN}"
+  echo "****"
+  NOTEBOOK_FLAGS="${NOTEBOOK_FLAGS} --NotebookApp.token=\"${CONTAINER_AUTH_TOKEN}\""
 fi
 
 chgrp docker /var/run/docker.sock
+service nginx start
 
 cd "/home/$NB_USER"
 chown -R "$NB_USER:users" .
