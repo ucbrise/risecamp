fe8d551b92a54b9aa1777742f1ff5fa62d616dd2
simon-mo <xmo@berkeley.edu>
2018-10-10 02:51:40 +0000

    Remove clipper specific script and fixes

--- base/risecamp_start.sh
+++ base/risecamp_start.sh
@@ -10,13 +10,6 @@ if [ "${CONTAINER_AUTH_TOKEN}" != "" ]; then
   NOTEBOOK_FLAGS="${NOTEBOOK_FLAGS} --NotebookApp.token=\"${CONTAINER_AUTH_TOKEN}\""
 fi
 
-
-IP=$(python -c "import requests; print(requests.get('http://ip.42.pl/raw').text)")
-GRAFANA_URL="http://"+IP+"/camp/clipper/grafana"
-docker stop clipper-grafana || true && docker rm clipper-grafana || true
-docker run --rm --name clipper-grafana --network clipper_network -d -e GF_DOMAIN=$IP -e GF_SERVER_ROOT_URL=$GRAFANA_URL grafana/grafana
-nginx -t
-
 chgrp docker /var/run/docker.sock
 service nginx start
 
--- clipper/clipper-init.sh
+++ clipper/clipper-init.sh
@@ -6,7 +6,8 @@ if [[ "$CAMP_HOST" = "" ]]; then
 fi
 
 GRAFANA_URL="http://$CAMP_HOST/camp/clipper/grafana"
+docker stop clipper-grafana || true
 docker run --rm --name clipper-grafana --network clipper_network --label ai.clipper.container.label -d -e GF_DOMAIN="$IP" -e GF_SERVER_ROOT_URL="$GRAFANA_URL" grafana/grafana
 
-mv /tmp/clipper.nginx.conf /etc/nginx/sites-enabled/default
+mv /home/clipper.nginx.conf /etc/nginx/sites-enabled/default
 service nginx restart
