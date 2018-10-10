53cec2d6c04fb6ef28bae01e7348662feb2970de
Simon Mo <xmo@berkeley.edu>
2018-10-09 23:05:12 -0700

    [Clipper] Create network on start

--- clipper/clipper-init.sh
+++ clipper/clipper-init.sh
@@ -7,6 +7,7 @@ fi
 
 GRAFANA_URL="http://$CAMP_HOST/camp/clipper/grafana"
 docker stop clipper-grafana || true
+docker network create clipper_network || true
 docker run --rm --name clipper-grafana --network clipper_network --label ai.clipper.container.label -d -e GF_DOMAIN="$IP" -e GF_SERVER_ROOT_URL="$GRAFANA_URL" grafana/grafana
 
 mv /home/clipper.nginx.conf /etc/nginx/sites-enabled/default
