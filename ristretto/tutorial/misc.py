4a5e467e9b69c00c5b3b714f7b2ed3a8cb64ae2f
erichson <erichson@uw.edu>
2018-10-27 12:52:32 -0700

    draft

--- ristretto/tutorial/misc.py
+++ ristretto/tutorial/misc.py
@@ -10,12 +10,104 @@ import cmocean
 import warnings; warnings.filterwarnings('ignore')
 
 
+import argparse
+import scipy.io as sio
+
+# Code adapted from Ryan Cooper (https://github.com/Coopss/EMNIST)
+def load_emnist(mat_file_path, width=28, height=28, max_=None, verbose=False):
+    ''' Load data in from .mat file as specified by the paper.
+        Arguments:
+            mat_file_path: path to the .mat, should be in sample/
+        Optional Arguments:
+            width: specified width
+            height: specified height
+            max_: the max number of samples to load
+            verbose: enable verbose printing
+        Returns:
+            A tuple of training and test data, and the mapping for class code to ascii value,
+            in the following format:
+                - ((training_images, training_labels), (testing_images, testing_labels), mapping)
+    '''
+    # Local functions
+    def rotate(img):
+        # Used to rotate images (for some reason they are transposed on read-in)
+        flipped = np.fliplr(img)
+        return np.rot90(flipped)
+
+    def display(img, threshold=0.5):
+        # Debugging only
+        render = ''
+        for row in img:
+            for col in row:
+                if col > threshold:
+                    render += '@'
+                else:
+                    render += '.'
+            render += '\n'
+        return render
+
+    # Load convoluted list structure form loadmat
+    mat = sio.loadmat(mat_file_path)
+
+    # Load char mapping
+    mapping = {kv[0]:kv[1:][0] for kv in mat['dataset'][0][0][2]}
+    #pickle.dump(mapping, open('mapping.p', 'wb' ))
+
+    # Load training data
+    if max_ == None:
+        max_ = len(mat['dataset'][0][0][0][0][0][0])
+    training_images = mat['dataset'][0][0][0][0][0][0][:max_].reshape(max_, height, width, 1)
+    training_labels = mat['dataset'][0][0][0][0][0][1][:max_].flatten()
+
+    # Load testing data
+    if max_ == None:
+        max_ = len(mat['dataset'][0][0][1][0][0][0])
+    else:
+        max_ = int(max_ / 6)
+    testing_images = mat['dataset'][0][0][1][0][0][0][:max_].reshape(max_, height, width, 1)
+    testing_labels = mat['dataset'][0][0][1][0][0][1][:max_].flatten()
+
+    # Reshape training data to be valid
+    if verbose == True: _len = len(training_images)
+    for i in range(len(training_images)):
+        if verbose == True: print('%d/%d (%.2lf%%)' % (i + 1, _len, ((i + 1)/_len) * 100), end='\r')
+        training_images[i] = rotate(training_images[i])
+    if verbose == True: print('')
+
+    # Reshape testing data to be valid
+    if verbose == True: _len = len(testing_images)
+    for i in range(len(testing_images)):
+        if verbose == True: print('%d/%d (%.2lf%%)' % (i + 1, _len, ((i + 1)/_len) * 100), end='\r')
+        testing_images[i] = rotate(testing_images[i])
+    if verbose == True: print('')
+
+    # Convert type to float32
+    training_images = training_images.astype('float32')
+    testing_images = testing_images.astype('float32')
+
+    # Normalize to prevent issues with model
+    training_images /= 255
+    testing_images /= 255
+    
+    training_images = training_images.reshape(112800,28*28)
+    testing_images = testing_images.reshape(18800,28*28)
+    
+    training_labels_char = []
+    for i in training_labels: 
+        training_labels_char.append(mapping[i].tostring().decode("utf-8"))
+    testing_labels_char = []
+    for i in testing_labels: 
+        testing_labels_char.append(mapping[i].tostring().decode("utf-8"))   
+    
+    return (training_images, training_labels, testing_images, testing_labels, training_labels_char, testing_labels_char)
+
+
 def plot_frames(input_data, x, y):    
     
     #******************************************************************************
     # Read land mask
     #******************************************************************************
-    data_land_mask = './data/lsmask.oisst.v2.nc'
+    data_land_mask = '/home/ben/Dropbox/Talks/Open Data/data/lsmask.oisst.v2.nc'
     ncLAND = Dataset(data_land_mask, mode='r')
     sst_land = np.asarray(ncLAND.variables['lsmask'][:].squeeze())
 
@@ -55,14 +147,14 @@ def plot_frames(input_data, x, y):
 
 def plot_components(B, x, y):
     # set up the figure
-    fig, ax = plt.subplots(x,y)
+    fig, ax = plt.subplots(x,y, figsize=(20, 10))
     
     # plot the digits: each image is 28x28 pixels
     mm = np.max(np.abs(B))
     index = 0
     for i in range(x):
         for j in range(y):
-            ax[i,j].imshow(B.T[index,:].reshape(28,28), cmap=plt.cm.gray, vmin=-mm, vmax=mm)
+            ax[i,j].imshow(B.T[index,:].reshape(28,28), cmap=cmocean.cm.delta, vmin=-mm, vmax=mm)
             ax[i,j].set_xticks([])
             ax[i,j].set_yticks([])
             index +=1
@@ -71,7 +163,7 @@ def plot_components(B, x, y):
     
 def plot_digits(features, target, x, y):    
     # set up the figure
-    fig, ax = plt.subplots(x,y)
+    fig, ax = plt.subplots(x,y, figsize=(20, 10))
     
     # plot the digits: each image is 28x28 pixels
     index = 0
@@ -85,6 +177,25 @@ def plot_digits(features, target, x, y):
             ax[i,j].text(0, 7, str(int(target[index])))
             index +=1
     plt.show()    
+    
+ 
+
+def plot_emnist(features, target, x, y):    
+    # set up the figure
+    fig, ax = plt.subplots(x,y, figsize=(20, 10))
+    
+    # plot the digits: each image is 28x28 pixels
+    index = 0
+    for i in range(x):
+        for j in range(y):
+            ax[i,j].imshow(features[index,:].reshape(28,28), cmap=plt.cm.binary)
+            ax[i,j].set_xticks([])
+            ax[i,j].set_yticks([])
+    
+            # label the image with the target value
+            ax[i,j].text(0, 7, str(target[index]))
+            index +=1
+    plt.show() 
     
     
     
