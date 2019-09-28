b5854c5c39427b2def3fbf00cd2f11dfc14ab943
root <root@ip-172-31-4-134.us-west-2.compute.internal>
2019-09-28 01:11:22 +0000

    Remove fedxgb

--- fedxgb/init-world.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This is an example startup script that performs critical initialization to
-# prepare the container environment:
-
-set -o errexit
-
-service ssh start
-echo "hello world!"
